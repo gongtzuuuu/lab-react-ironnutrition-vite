793be9d8cca48731c4d362430662c17edac0dbd0
Liz <claireliang2012@gmail.com>
2023-05-10 23:41:48 +0800

    Finish iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import "./App.css";
 import foodsData from "./foods.json";
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 import { Row, Divider, Button } from "antd";
+import { useState } from "react";
 
 function App() {
+  const [food, setFood] = useState(foodsData);
+  const [prevFood, setPrevFood] = useState(foodsData);
+  //setPrevFood(foodsData);
+  // console.log("newFood", newFood);
+
   return (
     <div className="App">
       <h1>Food List</h1>
+      <AddFoodForm
+        setFood={setFood}
+        prevFood={prevFood}
+        setPrevFood={setPrevFood}
+      />
 
-      <Button> Hide Form </Button>
-      <Button> Add New Food </Button>
+      <Button> Hide Form / Add New Food</Button>
 
       {/* Display Search component here */}
 
       <Divider>Food List</Divider>
       <Row style={{ width: "100%", justifyContent: "center" }}>
-        {foodsData.map((eachFood) => {
-          return (
-            <FoodBox
-              food={{
-                name: eachFood.name,
-                calories: eachFood.calories,
-                image: eachFood.image,
-                servings: eachFood.servings,
-              }}
-            />
-          );
+        {food.map((eachFood) => {
+          return <FoodBox food={eachFood} />;
         })}
       </Row>
     </div>
---
+++ src/components/AddFoodForm.jsx
@@ -0,0 +1,82 @@
+import { Divider, Input } from "antd";
+import { useState } from "react";
+
+// Iteration 4
+function AddFoodForm({ setFood, prevFood, setPrevFood }) {
+  // const [newFood, setNewFood] = useState();
+  const [name, setFoodName] = useState("");
+  const [image, setFoodImage] = useState("");
+  const [calories, setFoodCalories] = useState(0);
+  const [servings, setFoodServings] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //console.log(event);
+    const newFood = [{ name, image, calories, servings }, ...prevFood];
+    console.log("newfood: ", newFood);
+    setFood(newFood);
+    setPrevFood(newFood);
+    setFoodName("");
+    setFoodImage("");
+    setFoodCalories(0);
+    setFoodServings(0);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="addFoodForm">
+      <Divider>Add Food Entry</Divider>
+
+      <label>
+        Name:
+        <Input
+          value={name}
+          name="name"
+          type="text"
+          onChange={(event) => {
+            setFoodName(event.target.value);
+          }}
+        />
+      </label>
+
+      <label>
+        Image:
+        <Input
+          value={image}
+          name="image"
+          type="text"
+          onChange={(event) => {
+            setFoodImage(event.target.value);
+          }}
+        />
+      </label>
+
+      <label>
+        Calories:
+        <Input
+          value={calories}
+          name="calories"
+          type="number"
+          onChange={(event) => {
+            setFoodCalories(event.target.value);
+          }}
+        />
+      </label>
+
+      <label>
+        Servings:
+        <Input
+          value={servings}
+          name="servings"
+          type="number"
+          onChange={(event) => {
+            setFoodServings(event.target.value);
+          }}
+        />
+      </label>
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;
